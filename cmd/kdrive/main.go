cd621b00cdb350feec7fa36e70d88e3647f9ebd7
Kevin Kusi <triplefinn@gmail.com>
2022-03-10 23:41:50 -0800

    connected to aws and monitoring folders

---
+++ cmd/kdrive/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/fsnotify/fsnotify"
+)
+
+func HasLocalFileChanged() {
+
+}
+
+func HasRemoteFileChanged() {
+
+}
+
+func SyncBetweenLocalAndCloud() {
+
+}
+
+func main() {
+	log.Println("Starting k-drive")
+	// Load the Shared AWS Configuration (~/.aws/config)
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create an Amazon S3 service client
+	client := s3.NewFromConfig(cfg)
+	output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: aws.String("k-drive123"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("first page results:")
+	for _, object := range output.Contents {
+		log.Printf("key=%s size=%d", aws.ToString(object.Key), object.Size)
+	}
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", event.Name)
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/planetsp/k-drive
+
+go 1.16
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.15.0 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.15.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.26.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.1
+)
